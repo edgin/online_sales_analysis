3a0f41c80cec8c0983a6b0fd9881f9ea83e87299
edgin <lulemat@gmail.com>
2025-02-03 21:02:48 +0100

    Alter products values and remove show and sum of products

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ from product_manager import ProductManager
 manager = ProductManager()
 
 product1 = Product("Laptop", 1, 999.99)
-product2 = Product("Smartphone", 2, 599.99)
+product2 = Product("Monitor", 4, 300)
 product3 = Product("Tablet", 3,  399)
 
 
 manager.add_product(product1)
 manager.add_product(product2)
 manager.add_product(product3)
-manager.show_products()
-manager.product_sum()
 manager.remove_product("Laptop")
